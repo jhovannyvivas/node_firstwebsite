f5bbc0f33c997053ef424c573d18f29968f055bf
Jhovanny Vivas <jhovannyvivas@gmail.com>
2022-02-04 23:43:59 -0500

    primer commit

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) =>
+{
+    res.render('index.html', { title: 'Fisrt Website' });
+})
+
+router.get('/contact', (req, res) =>
+{
+    res.render('contact.html', { title: 'Contact Page' });
+})
+
+
+module.exports = router;
